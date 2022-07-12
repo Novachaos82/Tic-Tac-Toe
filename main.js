c37608a44774f0f7d81d3faf625c929eb0e564c8
Bhavesh Dewangan <ybkdddd@gmail.com>
2022-07-13 04:37:17 +0530

    add win check

--- main.js
+++ main.js
@@ -43,14 +43,14 @@ const boxes = document.querySelectorAll(".boxes");
 })();
 
 const winConditions = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-  [1, 5, 9],
-  [3, 5, 7],
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
   [1, 4, 7],
   [2, 5, 8],
-  [3, 6, 9],
+  [0, 4, 8],
+  [2, 4, 6],
 ];
 
 function mark() {
@@ -66,24 +66,36 @@ function mark() {
         player2Mark(box);
       }
       turn++;
-      wincheck(box);
+      //console.log(boxes[2].textContent === "X");
+      console.log(wincheck(boxes));
     });
   });
 }
-function wincheck(box) {
-  let count = 0;
-  for (let i = 0; i < winConditions.length; i++) {
-    for (let j = 0; j < 3; j++) {
-      if (box.dataset.index === winConditions[i][j]) {
-        console.log(winConditions[i][j]);
-        count++;
-      }
-    }
-  }
-  if (count === 3) {
-    console.log("won");
-  }
-  count = 0;
+//function wincheck(box) {
+//  let count = 0;
+//  for (let i = 0; i < winConditions.length; i++) {
+//    for (let j = 0; j < 3; j++) {
+//      if (box.dataset.index === winConditions[i][j]) {
+//        console.log(winConditions[i][j]);
+//        count++;
+//      }
+//    }
+//  }
+//  if (count === 3) {
+//    console.log("won");
+//  }
+//  count = 0;
+//}
+
+function wincheck(boxes) {
+  return winConditions.some((combination) => {
+    //console.log(combination);
+    return combination.every((i) => {
+      //console.log(i);
+      return boxes[i].textContent === "X";
+      //console.log("check");
+    });
+  });
 }
 
 function player1Mark(box) {
